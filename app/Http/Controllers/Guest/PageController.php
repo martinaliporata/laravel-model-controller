9bb098dbb3c876b2e1f03c047aefefc614ce3231
martinaliporata <martinaliporata@gmail.com>
2024-07-19 19:57:57 +0200

    data passed from db and added to view

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function index(){
+        // select * from movies
         $movies=Movie::all();
         return view('pages.index', compact('movies'));
     }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,15 @@
 @yield('page-title', 'homepage')
 
 @section('main-content')
-    <div>
-    </div>
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                Date: {{$movie->date}}
+                Nationality: {{$movie->nationality}}
+                Original title: {{$movie->original_title}}
+                Title: {{$movie->title}}
+                Vote: {{$movie->vote}}
+            </li>
+        @endforeach
+    </ul>
 @endsection
